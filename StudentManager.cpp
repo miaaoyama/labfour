7c9c54d936edae9eb78778f51e02b1e49ab48094
Alex <84359295+salt4king@users.noreply.github.com>
2026-09-22 16:50:45 -0700

    Remove removeById function from StudentManager
    Removed the removeById function from StudentManager.

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -1,14 +1,2 @@
 #include "StudentManager.h"
-#include <algorithm>
 
-bool StudentManager::removeById(int id) {
-    auto it = std::remove_if(students.begin(), students.end(),
-        [id](const Student& s) { return s.getId() == id; });
-
-    if (it == students.end()) {
-        return false; // no student had that id
-    }
-
-    students.erase(it, students.end());
-    return true;
-}
